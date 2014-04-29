8597640c316e7464bae2efec04585469cd0f9236
Suresh Balla <suresh.balla@outlook.com>
2014-04-29 19:13:25 +0530

    using env port

--- app.js
+++ app.js
@@ -31,5 +31,5 @@ app.configure('development', function() {
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 require('./routes')(app, config.server.useCors);
-app.listen(config.server.port);
+app.listen(process.env.PORT);
 console.log('Express server listening on port ' + config.server.port);
